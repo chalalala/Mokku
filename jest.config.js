506d409efb104f1010ec6242f838fa815544ee30
Mai Doan <doantuyetmai2000@gmail.com>
2025-03-17 22:52:52 +0700

    feat: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-    "roots": [
-        "src"
-    ],
-    "transform": {
-        "^.+\\.ts$": "ts-jest"
-    },
-}; 
+  roots: ["src"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  testEnvironment: "jest-environment-jsdom",
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+};
