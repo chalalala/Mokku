1c602df95cbbad6d82fbb18278374f52a3df3b64
Mai Doan <doantuyetmai2000@gmail.com>
2025-04-10 14:48:57 +0700

    feat: implement filter by active state

--- src/panel/App/Header/index.tsx
+++ src/panel/App/Header/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { shallow } from "zustand/shallow";
-import { Tabs, Flex, createStyles, Input, Button } from "@mantine/core";
+import { Tabs, Flex, createStyles, Input, Button, Select } from "@mantine/core";
 import { MdAdd } from "react-icons/md";
 import { TbSearch } from "react-icons/tb";
 import {
@@ -8,6 +8,7 @@ import {
   useGlobalStore,
   ViewEnum,
   useGlobalStoreState,
+  FilterEnum,
 } from "../store";
 import { ThemeButton } from "./ThemeButton";
 import { RefreshButton } from "./RefreshButton";
@@ -23,13 +24,19 @@ const viewSelector = (state: useGlobalStoreState) => ({
   setView: state.setView,
   search: state.search,
   setSearch: state.setSearch,
+  filter: state.filter,
+  setFilter: state.setFilter,
 });
 
 export const Header = () => {
-  const { view, setView, search, setSearch } = useGlobalStore(
-    viewSelector,
-    shallow,
-  );
+  const {
+    view,
+    setView,
+    search,
+    setSearch,
+    filter,
+    setFilter,
+  } = useGlobalStore(viewSelector, shallow);
   const setSelectedMock = useChromeStore((state) => state.setSelectedMock);
   const [showSupportUs, setShowSupportUs] = useState(false);
 
@@ -55,6 +62,21 @@ export const Header = () => {
                 size="xs"
                 defaultValue={search}
                 onChange={(event) => setSearch(event.target.value)}
+              />
+              <Select
+                size="xs"
+                defaultValue={filter}
+                onChange={(value) => {
+                  if (value) {
+                    setFilter(value);
+                  }
+                }}
+                data={[
+                  { value: FilterEnum.ALL, label: "All" },
+                  { value: FilterEnum.ACTIVE, label: "Active" },
+                  { value: FilterEnum.INACTIVE, label: "Inactive" },
+                ]}
+                wrapperProps={{ style: { width: "90px" } }}
               />
               <RecordButton />
               {view === "LOGS" ? <ClearButton /> : null}
--- src/panel/App/store/useGlobalStore.ts
+++ src/panel/App/store/useGlobalStore.ts
@@ -5,11 +5,19 @@ export enum ViewEnum {
   LOGS = "LOGS",
 }
 
+export const enum FilterEnum {
+  ALL = "all",
+  ACTIVE = "active",
+  INACTIVE = "inactive",
+}
+
 export type useGlobalStoreState = {
   view: "MOCKS" | "LOGS";
   setView: (view: ViewEnum) => void;
   search: string;
   setSearch: (search: string) => void;
+  filter: string;
+  setFilter: (filter: string) => void;
   recording: boolean;
   toggleRecording: () => void;
   meta: {
@@ -34,5 +42,7 @@ export const useGlobalStore = create<useGlobalStoreState>((set, get) => ({
     host: "",
     storeKey: "",
   },
+  filter: FilterEnum.ALL,
+  setFilter: (filter: string) => set({ filter }),
   setMeta: (meta) => set({ meta: meta }),
 }));
